ba51bd6d6206d322f0c8a9ca706e3e659fff054c
Takashi Furuya <takfuruya@gmail.com>
2013-05-22 16:21:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiApp::Application.config.secret_token = '3e01e7df3b78d5bd71ef1f58ad35108e5aaca94768dca0afafd1e8d63565a36d65a8a54ccdbd9794e41121a0f538b9fac4ba205bfa95f3017cbbce91e2a18cb9'
