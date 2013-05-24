90ee8ae1aa598bd4db83c6bab07a9fb235cc5054
Takashi Furuya <takfuruya@gmail.com>
2013-05-24 18:38:39 -0400

    Finish static home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,14 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Eqlea'" do
+    it "should have the h1 'Eqlea'" do
       visit '/static_pages/home'
-      page.should have_content('Eqlea')
+      page.should have_selector('h1', :text => 'Eqlea')
+    end
+    
+    it "should have the title 'Eqlea'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Eqlea")
     end
   end
 end
