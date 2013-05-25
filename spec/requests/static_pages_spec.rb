a3bb0089a615ffd6a7a865d2639ec8773b247dd3
Takashi Furuya <takfuruya@gmail.com>
2013-05-24 22:33:02 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,11 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the h1 'Eqlea'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Eqlea')
-    end
-    
-    it "should have the title 'Eqlea'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Eqlea")
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('title', :text => "Eqlea") }
   end
 end
