59af03f7a136ccc12a7b30abb4e09584b26c45f6
Takashi Furuya <takfuruya@gmail.com>
2013-07-21 11:17:05 -0400

    Change content format from string to text in notes db table

---
+++ db/migrate/20130721150724_change_content_format_in_notes.rb
@@ -0,0 +1,9 @@
+class ChangeContentFormatInNotes < ActiveRecord::Migration
+  def up
+    change_column :notes, :content, :text
+  end
+
+  def down
+    change_column :notes, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710235054) do
+ActiveRecord::Schema.define(:version => 20130721150724) do
 
   create_table "notes", :force => true do |t|
-    t.string   "content"
+    t.text     "content",    :limit => 255
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.string   "name"
     t.string   "public_id"
   end
